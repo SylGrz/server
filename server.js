0061ad1d2682b7751910608e2b4e78e573ef74de
SylGrz <103308904+SylGrz@users.noreply.github.com>
2022-04-09 14:36:21 +0200

    modified serve.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('SylieGie jest super SUPER!');
+  res.end('Aplikacja SyliGie jest SUPER!');
 }
 const server = http.createServer(requestListener);
 server.listen(port);
--- serwe.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const port = process.env.PORT;
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Aplikacja wyszła SUPER!');
-}
-const server = http.createServer(requestListener);
-server.listen(port);
